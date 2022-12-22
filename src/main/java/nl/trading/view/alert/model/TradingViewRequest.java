882db5c72c12d628a2da98878fa4a658600294b0
Pratik Mehta <pratikm121@gmail.com>
2022-12-22 11:15:28 +0100

    Update TradingViewRequest.java

--- src/main/java/nl/trading/view/alert/model/TradingViewRequest.java
+++ src/main/java/nl/trading/view/alert/model/TradingViewRequest.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 @Data
 public class TradingViewRequest implements Serializable {
+    private String user;
     private double price;
     private String ticker;
     private String timestamp;
