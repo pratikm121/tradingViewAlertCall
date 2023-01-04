09deb8924770bebc70f80d86cf127039612b7747
Pratik Mehta <pratikm121@gmail.com>
2023-01-04 17:10:44 +0100

    Update TradingViewController.java

--- src/main/java/nl/trading/view/alert/controller/TradingViewController.java
+++ src/main/java/nl/trading/view/alert/controller/TradingViewController.java
@@ -1,9 +1,6 @@
 package nl.trading.view.alert.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import nl.trading.view.alert.model.TradingViewRequest;
 import nl.trading.view.alert.service.TwilioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,11 +21,8 @@ public class TradingViewController {
     }
 
     @PostMapping("/alert") //http://localhost:7979/trading-view/alert
-    public void makeAlertCall(@RequestBody String request) throws JsonProcessingException {
+    public void makeAlertCall(@RequestBody String request) {
         log.info("Request {}",request);
-        ObjectMapper mapper = new ObjectMapper();
-        TradingViewRequest tradingViewRequest = mapper.readValue(request, TradingViewRequest.class);
-        log.info("TradingViewRequest {}",tradingViewRequest);
         this.twilioService.makeCall(request);
     }
 }
